56c1f94650d913352d844c05d08fa822918a6163
Devanshu Singh <75822824+Devanshu-singh-VR@users.noreply.github.com>
2021-11-10 10:34:46 +0530

    Delete OpenCV directory

--- OpenCV/1. open & save img.py
+++
@@ -1,7 +0,0 @@
-import cv2
-img = cv2.imread('maa.png',1)
-
-cv2.imshow('image',img)
-cv2.waitKey(5000) # wait for image to load (5000) is 5 sec for image to show
-
-cv2.imwrite('new.jpg',img) # save an image
